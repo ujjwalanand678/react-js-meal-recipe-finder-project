2f8edef74d1f451d189d7818d2fad12b25084984
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-06-26 17:22:47 +0530

    banner

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-
+import banner from "../assets/banner-2.png"
 const Home = () => {
-  return <div>Home</div>;
+  return <div>
+    <img src={banner} alt="banner" className="w-full h-[400px]"/>
+    </div>;
 };
 
 export default Home;
