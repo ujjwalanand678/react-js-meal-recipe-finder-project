ae9e958dc4d198a7cc9e0b01737f563a91220b64
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-06-26 18:33:35 +0530

    banner changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import banner from "../assets/banner-3.png"
+import Search from "./Search";
 const Home = () => {
   return (
-  <div>
+    <>
+     <div>
     <img src={banner} alt="banner" className="w-full h-[500px] brightness-40 z-0 relative bottom-10 "/>
     </div>
-    
+    <Search/>
+    </>
+ 
     );
 };
 
---
+++ src/Components/Search.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { IoSearchSharp } from "react-icons/io5";
+
+const Search = () => {
+  return (
+    <>
+      <div className="flex-col relative bottom-90">
+        <form className="flex place-content-center mb-5 ">
+          <input
+            className="bg-gray-200 rounded-full p-3 px-6 me-3 outline-none"
+            type="search"
+            placeholder="Search"
+          />
+          <button
+            className=" bg-gray-200 rounded-full p-3 px-4 text-amber-600 text-lg cursor-pointer"
+            type="submit"
+          >
+            <IoSearchSharp />
+          </button>
+        </form>
+        <p className="text-center text-3xl font-bold text-white mb-4">What are your favorite cuisines?</p>
+        <p className="text-center text-white">PERSONALIZE YOUR EXPERINCE</p>
+      </div>
+    </>
+  );
+};
+
+export default Search;
