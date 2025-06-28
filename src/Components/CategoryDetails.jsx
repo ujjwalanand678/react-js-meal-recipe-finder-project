24505c12ea9d44fc42f5c79c5ac2e17b4a4f4b90
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-06-28 17:25:45 +0530

    category detail made

--- src/Components/CategoryDetails.jsx
+++ src/Components/CategoryDetails.jsx
@@ -2,31 +2,78 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Search from "./Search";
-import Category from "./Category";
 import Banner from "./Banner";
 import SearchDetail from "./SearchDetail";
 
 const CategoryDetails = () => {
-    const {strCategory} = useParams()
-    const [mealCategory , setMealCategory] = useState({})
-    const [foodName, setFoodName] = useState("");
+  const [card, setCard] = useState([]);
+  const { strCategory } = useParams();
+  const [mealCategory, setMealCategory] = useState([]);
+  const [foodName, setFoodName] = useState("");
 
-    useEffect(()=>{
-        getCategoryData()
-    },[])
-    const getCategoryData = async()=>{
-        const {data} = await axios(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${strCategory}`)
-        setMealCategory(data.meals)
-        console.log(data.meals)
-    }
-    
+  const getCardData = async () => {
+    const { data } = await axios(
+      "https://www.themealdb.com/api/json/v1/1/categories.php"
+    );
+    setCard(data.categories);
+    console.log(data.categories);
+  };
+  useEffect(() => {
+    getCardData();
+    getCategoryData();
+  }, [strCategory]);
+  const getCategoryData = async () => {
+    const { data } = await axios(
+      `https://www.themealdb.com/api/json/v1/1/filter.php?c=${strCategory}`
+    );
+    setMealCategory(data.meals);
+    console.log(data.meals);
+  };
+  const cardDetails = card.find((cat) => cat.strCategory === strCategory);
   return (
-  <>
+    <>
       <Banner />
       <Search setFoodName={setFoodName} />
       <SearchDetail foodName={foodName} />
-      <Category />
-    </>);
+      <div className="relative bottom-50 mx-10">
+        {cardDetails && (
+          <div className="border-2 border-amber-600 p-7">
+            <p className="text-amber-600 font-bold text-3xl mb-2">
+              {cardDetails.strCategory}
+            </p>
+            <p className="text-lg text-gray-600">
+              {cardDetails.strCategoryDescription}
+            </p>
+          </div>
+        )}
+      </div>
+      <div className="relative bottom-40 mx-3">
+        <p className="font-bold mx-7 text-xl ">MEALS</p>
+        <hr className="text-amber-600 w-10 border-2 mx-7" />
+        <div className="grid grid-cols-5 mt-10 mx-4">
+          {mealCategory && Object.keys(mealCategory).length > 0 ? (
+            mealCategory.map((meal) => (
+              <div
+                className=" m-3 mb-10 bg-white rounded-md shadow-lg"
+                key={meal.idMeal}
+              >
+                <img
+                  src={meal.strMealThumb}
+                  alt={meal.strMeal}
+                  className="w-full flex relative bottom-6 rounded-t-md"
+                />
+                <p className="relative bottom-5 px-3 font-bold">
+                  {meal.strMeal}
+                </p>
+              </div>
+            ))
+          ) : (
+            <strong className="text-center">LOADING</strong>
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CategoryDetails;
--- src/Components/SearchDetail.jsx
+++ src/Components/SearchDetail.jsx
@@ -16,15 +16,21 @@ const SearchDetail = ({ foodName }) => {
     console.log(data.meals);
   };
   return (
-    <div className="relative bottom-30 " >
-      <h1 className="font-bold mx-7">MEALS</h1>
-      <hr className="text-amber-600 w-10 border-2 mx-7" />
+    <div className="relative bottom-30 ">
+      {mealsName.length > 0 ? (
+        <>
+          <h1 className="font-bold mx-7">MEALS</h1>
+          <hr className="text-amber-600 w-10 border-2 mx-7" />
+        </>
+      ) : null}
 
       <div className="grid grid-cols-5 gap-4 m-7">
-        {mealsName.length > 0 ? (
-          mealsName.map((food) => (
-            <>
-              <div className="m-1 bg-white rounded-md shadow-lg" key={food.idMeal}>
+        {mealsName.length > 0
+          ? mealsName.map((food) => (
+              <div
+                className="m-1 bg-white rounded-md shadow-lg"
+                key={food.idMeal}
+              >
                 <p className="text-amber-600 px-3 bg-white rounded-full justify-self-end relative top-3 right-3 z-10">
                   {food.strCategory}
                 </p>
@@ -33,14 +39,15 @@ const SearchDetail = ({ foodName }) => {
                   alt={food.strMeal}
                   className="w-full flex relative bottom-6 rounded-t-md"
                 />
-                <p className="relative bottom-5 px-3 text-gray-500">{food.strArea}</p>
-                <p className="relative bottom-5 px-3 font-bold" >{food.strMeal}</p>
+                <p className="relative bottom-5 px-3 text-gray-500">
+                  {food.strArea}
+                </p>
+                <p className="relative bottom-5 px-3 font-bold">
+                  {food.strMeal}
+                </p>
               </div>
-            </>
-          ))
-        ) : (
-          <p></p>
-        )}
+            ))
+          : null}
       </div>
     </div>
   );
